68d25bcc876fc3144fbef474e309aba30287c7a2
MukulVerma14 <mukul23102@iiitnr.edu.in>
2026-06-17 17:44:06 +0530

    Initial commit

---
+++ src/test/java/com/example/ailmc/AilmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ailmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AilmcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
